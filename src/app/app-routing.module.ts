f0761228e3fb61eeea1ff12974fa35d4a077fa37
Piotr Kieltyka <p.kieltyka@gmail.com>
2021-07-13 19:40:45 +0200

    add components, routes and contact form example

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AboutUsComponent} from "./about-us/about-us.component";
+import {TeamComponent} from "./team/team.component";
+import {ContactComponent} from "./contact/contact.component";
+import {TermsComponent} from "./terms/terms.component";
+import {HomeComponent} from "./home/home.component";
 
-const routes: Routes = [];
+const siteRoutes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'aboutUs', component: AboutUsComponent},
+  {path: 'team', component: TeamComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'terms', component: TermsComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '**', redirectTo: 'home'},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(siteRoutes, {
+    scrollPositionRestoration: 'top',
+  })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { TeamComponent } from './team/team.component';
+import { ContactComponent } from './contact/contact.component';
+import { TermsComponent } from './terms/terms.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    TeamComponent,
+    ContactComponent,
+    TermsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
