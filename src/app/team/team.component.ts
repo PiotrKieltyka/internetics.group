88275c06f46e41f471c93180f8aa8b5676debf8a
Piotr Kieltyka <p.kieltyka@gmail.com>
2021-08-05 18:02:51 +0200

    add team cards & translations

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
   styleUrls: ['./team.component.scss']
 })
-export class TeamComponent implements OnInit {
+export class TeamComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  team: Array<{
+    name: string;
+    socialtag: string;
+    occupation: string;
+    codingFrom: string;
+    bio: string;
+  }> = [
+    {
+      name: 'Piotr Kiełtyka',
+      socialtag: '@fangstick',
+      occupation: 'Fullstack Developer',
+      codingFrom: 'Coding from Planet Earth',
+      bio: 'Information Technology Engineer with many years of experience in maintenance and deployment of hardware and software as well as software development. Dedicated developer with a passion for coding and problem solving.'
+    },
+    {
+      name: 'Piotr Kiełtyka',
+      socialtag: '@fangstick',
+      occupation: 'Fullstack Developer',
+      codingFrom: 'Coding from Planet Earth',
+      bio: 'Information Technology Engineer with many years of experience in maintenance and deployment of hardware and software as well as software development. Dedicated developer with a passion for coding and problem solving.'
+    }
+  ]
 
 }
