99e6dad07a4b37c6b99c3098412ad78dc7b723e9
Piotr Kieltyka <p.kieltyka@gmail.com>
2021-08-05 15:52:20 +0200

    add multi language

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'internetics-group';
+
+  constructor(
+    private translateService: TranslateService,
+  ) {
+    translateService.setDefaultLang('en');
+  }
+
+  switchLanguage(language: string) {
+    this.translateService.use(language);
+  }
+
 }
