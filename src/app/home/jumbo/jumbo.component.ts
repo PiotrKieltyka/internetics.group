a663510f7f8946e21e8259675bb6538989d64986
Piotr Kieltyka <p.kieltyka@gmail.com>
2021-08-20 15:28:33 +0200

    optimize images

--- src/app/home/jumbo/jumbo.component.ts
+++ src/app/home/jumbo/jumbo.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-jumbo',
   templateUrl: './jumbo.component.html',
   styleUrls: ['./jumbo.component.scss']
 })
-export class JumboComponent implements OnInit {
+export class JumboComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  // changeStaticImagesToAnimations(): void {
+  //   const jumboImage1 = document.querySelector('#jumbo-image-1') as HTMLImageElement;
+  //   const jumboImage2 = document.querySelector('#jumbo-image-2') as HTMLImageElement;
+  //   const jumboGif1 = new Image();
+  //   const jumboGif2 = new Image();
+  //
+  //   jumboGif1.src = "./assets/video/HunanGSX/earth-zoom.webp";
+  //   jumboGif1.addEventListener('load', () => {
+  //     jumboImage1.src = jumboGif1.src;
+  //   })
+  //
+  //   jumboGif2.src = "./assets/video/HunanGSX/my-diamo-loop-2.webp";
+  //   jumboGif2.addEventListener('load', () => {
+  //     jumboImage2.src = jumboGif2.src;
+  //   })
+  // }
 
 }
