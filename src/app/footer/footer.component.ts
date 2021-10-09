1494d650197c9b42ebdb270c95259612c41cfb2d
Piotr Kieltyka <p.kieltyka@gmail.com>
2021-10-09 15:46:50 +0200

    add footer to home and contact page

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public route: ActivatedRoute,
+  ) {
+    console.log(route);
+  }
 
   ngOnInit(): void {
   }
