2eda349ede9792abcfd4144aea1aad576779699c
Piotr Kieltyka <p.kieltyka@gmail.com>
2021-10-26 09:16:48 +0200

    refactory jumbo component

--- src/app/home/jumbo/jumbo.component.ts
+++ src/app/home/jumbo/jumbo.component.ts
@@ -8,21 +8,39 @@ import { HttpResponse } from '@angular/common/http';
   styleUrls: ['./jumbo.component.scss'],
 })
 export class JumboComponent implements AfterViewInit {
-  constructor(private loadImageService: LoadImageService) {}
+  jumboItems: Array<{
+    imageUrl: string;
+    header: string;
+    content: string;
+  }> = [
+    {
+      imageUrl: './assets/video/HunanGSX/earth-zoom-screen.webp',
+      header: 'FirstJumboHeader',
+      content: 'FirstWelcomeMessage',
+    },
+    {
+      imageUrl: './assets/video/HunanGSX/my-diamo-loop-screen.webp',
+      header: 'SecondJumboHeader',
+      content: 'SecondWelcomeMessage',
+    },
+  ];
+
+  constructor(private loadImageService: LoadImageService) {
+  }
 
   ngAfterViewInit() {
+    const jumboImage0 = document.querySelector(
+      '#jumbo-image-0',
+    ) as HTMLImageElement;
     const jumboImage1 = document.querySelector(
       '#jumbo-image-1',
     ) as HTMLImageElement;
-    const jumboImage2 = document.querySelector(
-      '#jumbo-image-2',
-    ) as HTMLImageElement;
 
-    if (this.checkFileName(jumboImage1.src) === 'earth-zoom-screen.webp') {
+    if (this.checkFileName(jumboImage0.src) === 'earth-zoom-screen.webp') {
       this.loadImageService
         .getImage('./assets/video/HunanGSX/earth-zoom.webp')
         .subscribe((item: HttpResponse<ArrayBuffer>): void => {
-          jumboImage1.src =
+          jumboImage0.src =
             'data:image/webp;base64,' +
             this.loadImageService.encodeArrayBufferToBase64(
               item.body!,
@@ -31,11 +49,11 @@ export class JumboComponent implements AfterViewInit {
             );
         });
     }
-    if (this.checkFileName(jumboImage2.src) === 'my-diamo-loop-screen.webp') {
+    if (this.checkFileName(jumboImage1.src) === 'my-diamo-loop-screen.webp') {
       this.loadImageService
         .getImage('./assets/video/HunanGSX/my-diamo-loop-2.webp')
         .subscribe((item: HttpResponse<ArrayBuffer>) => {
-          jumboImage2.src =
+          jumboImage1.src =
             'data:image/webp;base64,' +
             this.loadImageService.encodeArrayBufferToBase64(
               item.body!,
